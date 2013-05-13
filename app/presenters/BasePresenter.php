e69ed4cc52f7393bd45fc52fc7edf1f7f436b3cf
Vlasta Neubauer <paranoiq@centrum.cz>
2013-05-13 20:02:19 +0200

    removed TemplateFactory

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,8 +8,21 @@ use Nette,
 
 /**
  * Base presenter for all application presenters.
+ *
+ * @property-read RepositoryContainer $orm
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+
+	/**
+	 * Returns RepositoryContainer.
+	 *
+	 * @author Jan Tvrdík
+	 * @return RepositoryContainer
+	 */
+	public function getOrm()
+	{
+		return $this->context->getService('repositoryContainer');
+	}
 
 }
