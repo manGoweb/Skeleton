Clevis/ClevisArticles

added TemplateFactory; BasePresenter, BaseControl moved to Clevispace namespace

app/bootstrap.php
@@ -18,7 +18,7 @@ $configurator->setTempDirectory(__DIR__ . '/../temp');
 // Enable RobotLoader - this will load all classes automatically
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../libs')
+	->addDirectory(__DIR__ . '/../libs/clevispace')
 	->register();
 
 // Create Dependency Injection container from config.neon file

manGoweb/Skeleton

app/bootstrap.php
@@ -15,6 +15,7 @@ $configurator->setTempDirectory(__DIR__ . '/../temp');
 $configurator->createRobotLoader()
 	->addDirectory(__DIR__)
 	->addDirectory(__DIR__ . '/../vendor/others')
+	->addDirectory(__DIR__ . '/../vendor/clevispace')
 	->register();
 
 $configurator->addConfig(__DIR__ . '/config/config.neon');