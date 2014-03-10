Mikulas/Skeleton21

added Configurator

app/bootstrap.php
@@ -4,26 +4,21 @@ use Nette\Diagnostics\Debugger;
 
 // Load Nette Framework or autoloader generated by Composer
 require __DIR__ . '/../libs/autoload.php';
+require __DIR__ . '/config/Configurator.php';
 
 
-$configurator = new Nette\Configurator;
+// Configure application
+$configurator = new App\Configurator(__DIR__ . '/../temp');
 
 // Enable Nette Debugger for error visualisation & logging
-//$configurator->setDebugMode(TRUE);
-$configurator->enableDebugger(Debugger::DETECT, __DIR__ . '/../log', 'error@clevis.cz');
-
-// Specify folder for cache
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+//$configurator->setDebugMode($configurator::AUTO);
+$configurator->enableDebugger(__DIR__ . '/../log', 'error@clevis.cz');
 
 // Enable RobotLoader - this will load all classes automatically
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../libs/clevispace')
-	->register();
+$loader = $configurator->createRobotLoader();
+$loader->register();
 
-// Create Dependency Injection container from config.neon file
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+// Create Dependency Injection container from configuration files
 $container = $configurator->createContainer();
 
 $container->application->catchExceptions = Debugger::$productionMode;

app/config/Configurator.php
@@ -1,0 +1,164 @@
+<?php
+
+namespace App;
+
+use Nette;
+use Nette\DI;
+use Nette\Config;
+use Nette\Loaders\RobotLoader;
+use Nette\FileNotFoundException;
+use RuntimeException;
+use SystemContainer;
+
+
+/**
+ * Nastavení aplikace (nahrazuje boostrap)
+ *
+ * @author Petr Procházka
+ * @author Vojtěch Dobeš
+ */
+class Configurator extends Config\Configurator
+{
+
+	/** @var array of function(Configurator $sender); Occurs before first Container is created */
+	public $onInit = array();
+
+	/** @var array of function(Configurator $sender); Occurs after first Container is created */
+	public $onAfter = array();
+
+
+
+	/**
+	 * @param  string|NULL null mean autodetect
+	 * @param  array|NULL
+	 */
+	public function __construct($tempDirectory = NULL, array $params = NULL)
+	{
+		parent::__construct();
+
+		if ($tempDirectory === NULL) {
+			$tempDirectory = realpath(__DIR__ . '/../../temp');
+		}
+		$this->addParameters((array) $params + array_map('realpath', array(
+			'appDir' => __DIR__ . '/..',
+			'libsDir' => __DIR__ . '/../../libs',
+			'wwwDir' => __DIR__ . '/../../www',
+		)));
+		$this->setTempDirectory($tempDirectory);
+
+		foreach (get_class_methods($this) as $name) {
+			if ($pos = strpos($name, 'onInit') === 0) {
+				$this->onInit[lcfirst(substr($name, $pos + 5))] = array($this, $name);
+			}
+		}
+
+		foreach (get_class_methods($this) as $name) {
+			if ($pos = strpos($name, 'onAfter') === 0) {
+				$this->onAfter[lcfirst(substr($name, $pos + 5))] = array($this, $name);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Zaregistruje konfigurační soubory
+	 */
+	public function onInitConfigs()
+	{
+		$params = $this->getParameters();
+		$this->addConfig($params['appDir'] . '/config/config.neon', FALSE);
+		$this->addConfig($params['appDir'] . '/config/config.local.neon', FALSE);
+	}
+
+
+
+	/**
+	 * Zaregistruje rozšíření konfigurace:
+	 * - dibi
+	 */
+	public function onInitExtensions()
+	{
+		$this->onCompile['dibi'] = function ($configurator, DI\Compiler $compiler) {
+			//$compiler->addExtension('dibi', new \DibiNetteExtension);
+		};
+	}
+
+	/**
+	 * Zaregistruje moduly
+	 */
+	public function onInitModules()
+	{
+		//App\Some\Module::register($this);
+	}
+
+	/**
+	 * Vytvoří RobotLoader a zaregistruje adresáře 'app' a 'libs/clevispace'
+	 *
+	 * @return RobotLoader
+	 */
+	public function createRobotLoader()
+	{
+		$params = $this->getParameters();
+		$loader = parent::createRobotLoader();
+		$loader->addDirectory($params['appDir']);
+		$loader->addDirectory($params['libsDir'] . '/clevispace');
+
+		return $loader;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getParameters()
+	{
+		return $this->parameters;
+	}
+
+	/**
+	 * Vytvoří systémový DI kontejner
+	 *
+	 * @return SystemContainer
+	 */
+	public function createContainer()
+	{
+		$this->onInit($this);
+		$this->onInit = array();
+
+		try {
+			$container = parent::createContainer();
+			$this->onAfter($container);
+
+			return $container;
+		}
+		catch (FileNotFoundException $e)
+		{
+			if (strpos($e->getMessage(), 'local') !== FALSE)
+			{
+				throw new MissingLocalConfigException($e);
+			}
+			else
+			{
+				throw $e;
+			}
+		}
+	}
+
+}
+
+
+/**
+ * Chybějící konfigurační soubor
+ */
+class MissingLocalConfigException extends RuntimeException
+{
+
+	/**
+	 * @param  FileNotFoundException
+	 */
+	public function __construct(FileNotFoundException $e)
+	{
+		parent::__construct('Pro spuštění aplikace si do složky "app/config" doplň konfigurační soubor "local.neon". Můžeš za tím účelem zkopírovat "config.local.example.neon", který se nenačítá a slouží jako vzor.', NULL, $e);
+	}
+
+}

manGoweb/Skeleton

app/bootstrap.php
@@ -1,26 +1,24 @@
 <?php
 
-require __DIR__ . '/shortcuts.php';
 use Nette\Diagnostics\Debugger;
 
+// Load Nette Framework or autoloader generated by Composer
 require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/config/Configurator.php';
 
-$configurator = new Nette\Configurator;
 
-//$configurator->setDebugMode(TRUE);
-$configurator->enableDebugger(Debugger::DETECT, __DIR__ . '/../log', 'error@example.cz');
+// Configure application
+$configurator = new App\Configurator(__DIR__ . '/../temp');
 
-$configurator->setTempDirectory(__DIR__ . '/../temp');
+// Enable Nette Debugger for error visualisation & logging
+//$configurator->setDebugMode($configurator::AUTO);
+$configurator->enableDebugger(__DIR__ . '/../log', 'error@example.cz');
 
-$configurator->createRobotLoader()
-	->addDirectory(__DIR__)
-	->addDirectory(__DIR__ . '/../vendor/others')
-	->addDirectory(__DIR__ . '/../vendor/clevispace')
-	->register();
+// Enable RobotLoader - this will load all classes automatically
+$loader = $configurator->createRobotLoader();
+$loader->register();
 
-$configurator->addConfig(__DIR__ . '/config/config.neon');
-$configurator->addConfig(__DIR__ . '/config/config.local.neon');
-
+// Create Dependency Injection container from configuration files
 $container = $configurator->createContainer();
 
 $container->application->catchExceptions = Debugger::$productionMode;

app/config/Configurator.php
@@ -1,0 +1,165 @@
+<?php
+
+namespace App;
+
+use Nette;
+use Nette\DI;
+use Nette\Config;
+use Nette\Loaders\RobotLoader;
+use Nette\FileNotFoundException;
+use RuntimeException;
+use SystemContainer;
+
+
+/**
+ * Nastavení aplikace (nahrazuje boostrap)
+ *
+ * @author Petr Procházka
+ * @author Vojtěch Dobeš
+ */
+class Configurator extends Config\Configurator
+{
+
+	/** @var array of function(Configurator $sender); Occurs before first Container is created */
+	public $onInit = array();
+
+	/** @var array of function(Configurator $sender); Occurs after first Container is created */
+	public $onAfter = array();
+
+
+
+	/**
+	 * @param  string|NULL null mean autodetect
+	 * @param  array|NULL
+	 */
+	public function __construct($tempDirectory = NULL, array $params = NULL)
+	{
+		parent::__construct();
+
+		if ($tempDirectory === NULL) {
+			$tempDirectory = realpath(__DIR__ . '/../../temp');
+		}
+		$this->addParameters((array) $params + array_map('realpath', array(
+			'appDir' => __DIR__ . '/..',
+			'libsDir' => __DIR__ . '/../../vendor',
+			'wwwDir' => __DIR__ . '/../../www',
+		)));
+		$this->setTempDirectory($tempDirectory);
+
+		foreach (get_class_methods($this) as $name) {
+			if ($pos = strpos($name, 'onInit') === 0) {
+				$this->onInit[lcfirst(substr($name, $pos + 5))] = array($this, $name);
+			}
+		}
+
+		foreach (get_class_methods($this) as $name) {
+			if ($pos = strpos($name, 'onAfter') === 0) {
+				$this->onAfter[lcfirst(substr($name, $pos + 5))] = array($this, $name);
+			}
+		}
+	}
+
+
+
+	/**
+	 * Zaregistruje konfigurační soubory
+	 */
+	public function onInitConfigs()
+	{
+		$params = $this->getParameters();
+		$this->addConfig($params['appDir'] . '/config/config.neon', FALSE);
+		$this->addConfig($params['appDir'] . '/config/config.local.neon', FALSE);
+	}
+
+
+
+	/**
+	 * Zaregistruje rozšíření konfigurace:
+	 * - dibi
+	 */
+	public function onInitExtensions()
+	{
+		$this->onCompile['dibi'] = function ($configurator, DI\Compiler $compiler) {
+			//$compiler->addExtension('dibi', new \DibiNetteExtension);
+		};
+	}
+
+	/**
+	 * Zaregistruje moduly
+	 */
+	public function onInitModules()
+	{
+		//App\Some\Module::register($this);
+	}
+
+	/**
+	 * Vytvoří RobotLoader a zaregistruje adresáře 'app' a 'libs/clevispace'
+	 *
+	 * @return RobotLoader
+	 */
+	public function createRobotLoader()
+	{
+		$params = $this->getParameters();
+		$loader = parent::createRobotLoader();
+		$loader->addDirectory($params['appDir']);
+		$loader->addDirectory($params['libsDir'] . '/others');
+		$loader->addDirectory($params['libsDir'] . '/clevispace');
+
+		return $loader;
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getParameters()
+	{
+		return $this->parameters;
+	}
+
+	/**
+	 * Vytvoří systémový DI kontejner
+	 *
+	 * @return SystemContainer
+	 */
+	public function createContainer()
+	{
+		$this->onInit($this);
+		$this->onInit = array();
+
+		try {
+			$container = parent::createContainer();
+			$this->onAfter($container);
+
+			return $container;
+		}
+		catch (FileNotFoundException $e)
+		{
+			if (strpos($e->getMessage(), 'local') !== FALSE)
+			{
+				throw new MissingLocalConfigException($e);
+			}
+			else
+			{
+				throw $e;
+			}
+		}
+	}
+
+}
+
+
+/**
+ * Chybějící konfigurační soubor
+ */
+class MissingLocalConfigException extends RuntimeException
+{
+
+	/**
+	 * @param  FileNotFoundException
+	 */
+	public function __construct(FileNotFoundException $e)
+	{
+		parent::__construct('Pro spuštění aplikace si do složky "app/config" doplň konfigurační soubor "local.neon". Můžeš za tím účelem zkopírovat "config.local.example.neon", který se nenačítá a slouží jako vzor.', NULL, $e);
+	}
+
+}