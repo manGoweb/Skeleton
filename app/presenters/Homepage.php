ab4ce2bf20e9d93b17fd0675c75185b21c860dfa
Mikulas <rullaf@gmail.com>
2014-06-26 17:12:46 +0200

    add intro homepage

--- app/presenters/Homepage.php
+++ app/presenters/Homepage.php
@@ -6,9 +6,14 @@ namespace App\Presenters;
 class Homepage extends Presenter
 {
 
-	public function actionDefault()
+	public function renderDefault()
 	{
-
+		$this->template->add('version', (object) [
+			'nette' => \Nette\Framework::VERSION,
+			'porm' => \Orm\Orm::VERSION,
+			'php' => PHP_VERSION,
+			'server' => isset($_SERVER['SERVER_SOFTWARE']) ? $_SERVER['SERVER_SOFTWARE'] : NULL,
+		]);
 	}
 
 }
