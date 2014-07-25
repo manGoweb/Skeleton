187efc11e4a8c76d13e8ae9cfb4c4a0e757d94e8
Mikulas <rullaf@gmail.com>
2014-07-25 20:31:55 +0300

    fix bin and app not working together due to fs acl
    Proper solution would be having separate caches for app and console, but beside other hinderances, cache dir is hardcoded into container creation, latte does not use cacheStorage service etc.
    
    See for discussion about umask https://github.com/nette/nette/issues/1039
    
    [Closes #10]

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -4,4 +4,5 @@ require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/exceptions.php';
 require __DIR__ . '/config/Configurator.php';
 
+umask(0);
 return (new \App\Config\Configurator())->createContainer();
