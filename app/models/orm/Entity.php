e4b9513f459447c25215a5eb5bfbae21cd48d074
Mikulas <rullaf@gmail.com>
2014-07-25 14:28:24 +0300

    add simplified fan annotation meta data for orm
    This allows annotations such as
    
        use App\Models\Rme;
        /** @property Rme\Foo $foo {1:m bars $foo} */
    
    instead of
    
        /** @property \App\Models\Rme\Foo $foo {1:m bars $foo} */
    
    [Closes #9]

--- app/models/orm/Entity.php
+++ app/models/orm/Entity.php
@@ -12,4 +12,14 @@ use Orm;
 abstract class Entity extends Orm\Entity
 {
 
+	/**
+	 * Adds support for FQN annotations
+	 * @param $entityClass
+	 * @return Orm\MetaData
+	 */
+	public static function createMetaData($entityClass)
+	{
+		return FQNAnnotationMetaData::getMetaData($entityClass);
+	}
+
 }
