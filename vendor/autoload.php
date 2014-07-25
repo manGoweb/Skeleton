9ac5b0bf82e249eb2a8eb08626e7d48af1c736d2
Mikulas <rullaf@gmail.com>
2014-07-25 21:10:37 +0300

    update symfony console v2.5.0 => v2.5.2

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit44f2d177bb83538801d7edda57a26b15::getLoader();
+return ComposerAutoloaderInitc9dde7b32b33ea65fdcf39a81f36e6cb::getLoader();
